b3eca94f21fcf15a93b743b82ac30d9cbddaaf6b
PushEax <pusheax.vk@gmail.com>
2025-03-19 15:02:43 +0200

    add TransactionHistory

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import Profile from '../Profile/Profile';
 import userData from '../../userData.json';
 import FriendList from '../FriendList/FriendList';
 import friends from '../../friends.json';
+import transactions from '../../transactions.json';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
